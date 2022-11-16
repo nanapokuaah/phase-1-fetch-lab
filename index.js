948c63a7eb56645a37d5b3b528b5ef78e42743be
Nana Adu Pokuaah <nana.pokuaah@bison.howard.edu>
2022-11-16 18:12:04 -0500

    commit tree 8328222024734b912097ccd7e189884df051b905

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+   return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((books) => renderBooks(books));
+}
+  function renderBooks(books) {
+  const main = document.querySelector("main");
+  books.forEach(books => {
+    const h2 = document.createElement("h2");
+    h2.innerHTML = books.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+document.addEventListener("DOMContentLoaded", function() {
+    fetchBooks();
 });
